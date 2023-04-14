8aaeacd039adb873eedbf4e4492da1c2081c7840
arbi <arbiihsan@gmail.com>
2023-04-14 11:16:06 +0700

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCATVs1OUrbOgMC3HGzr7Tbow1NoMPCx-Y",
+    authDomain: "netflix-clone-react-21bb0.firebaseapp.com",
+    projectId: "netflix-clone-react-21bb0",
+    storageBucket: "netflix-clone-react-21bb0.appspot.com",
+    messagingSenderId: "1096090152207",
+    appId: "1:1096090152207:web:9151a32a8e86288ae7a27a"
+  };
